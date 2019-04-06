480e22fbec78efc9f57682da59cfa7c7d524275f
Xohan Otero Barbosa <xohan.otero@gmail.com>
2019-04-06 15:12:37 +0200

    Set sleep time to 1h and small fixes
    Cant read actually % of humidity, just tells us if its HIGH or LOW, sends email only when its low.

--- code/soil.py
+++ code/soil.py
@@ -2,54 +2,54 @@
 import RPi.GPIO as GPIO
 import time
 import smtplib
-
+ 
 #GPIO SETUP
 channel = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
-water_threshhold = 150 #The values go between 0 and 1024 meaning that 150 is aprox 15%
-#Email SETUP
-smtp_username = "" # This is the username used to login to your SMTP provider
-smtp_password = "" # This is the password used to login to your SMTP provider
-smtp_host = "smtp.gmail.com" # This is the host of the SMTP provider
-smtp_port = 587 # This is the port that your SMTP provider uses
+
+#Email Setup
+smtp_username = ""#the address the email is being sent from
+smtp_password = ""#password to that email
+smtp_host = "smtp.gmail.com" #smtp server for your email service (this case gmail)
+smtp_port = 587 #port for the smtp server(This case gmail)
 
 smtp_sender = "" # This is the FROM email address
-smtp_receivers = [''] # This is the TO email address
+smtp_receivers = "" # This is the TO email address
 #change reciever email above and below on both variables
-message_dead = """From: Sender Name <uia.botanica1@gmail.com>
-To: Receiver Name <>
-Subject: Moisture Sensor Notification
-Warning, no moisture detected! Plant death imminent!!! :'(
-"""
+message_dead = "Needs Water"
 
 # This is the message that will be sent when moisture IS detected again
 
-message_alive = """From: Sender Name <uia.botanica1@gmail.com>
-To: Receiver Name <>
-Subject: Moisture Sensor Notification
-Panic over! Plant has water again :)
-"""
+
 def sendEmail(smtp_message):
-	try:
-		smtpObj = smtplib.SMTP(smtp_host, smtp_port)
-		smtpObj.login(smtp_username, smtp_password) # If you don't need to login to your smtp provider, simply remove this line
-		smtpObj.sendmail(smtp_sender, smtp_receivers, smtp_message)
-		print ("Successfully sent email")
-	except smtplib.SMTPException:
-		print ("Error: unable to send email")
+    try:
+        smtpObj = smtplib.SMTP(smtp_host, smtp_port)
+        smtpObj.ehlo()
+        smtpObj.starttls()
+        smtpObj.login(smtp_username, smtp_password) # If you don't need to login to your smtp provider, simply remove this line
+        smtpObj.sendmail(smtp_sender, smtp_recievers, smtp_message)
+        smtpObj.quit()
+        print ("Successfully sent email")
+    except smtplib.SMTPException:
+        print ("Error: unable to send email")
+
+
 
 def callback(channel):
-        if GPIO.input(channel) > water_threshhold:
-                print ("Water Detected!" + GPIO.input(channel))
-                sendEmail:message_dead
+        if GPIO.input(channel):
+                str(GPIO.input(channel))
+                print ("Water Detected!")
+                
+                
         else:
-                print ("Water not Detected!")
-                sendEmail:message_alive
-
+                print ("Water Not Detected!")
+                
+                sendEmail(message_dead)
+ 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
-
+ 
 # infinite loop
 while True:
-        time.sleep(60)
+        time.sleep(3600)
